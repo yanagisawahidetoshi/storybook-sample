586fcff6bd3ac111f8036e9e6d5a434a3faa574a
joko.ippei <joko.ippei@raccoon.ne.jp>
2025-01-28 10:27:39 +0900

    ログイン情報のあるヘッダーの作成

---
+++ src/stories/userHeader.stories.js
@@ -0,0 +1,42 @@
+import userHeader from './userHeader.vue'
+
+export default {
+  title: 'Example/userHeader',
+  component: userHeader,
+  parameters: {
+    layout: 'fullscreen'
+  }
+}
+
+export const PreLogin = {
+  args: {
+    user: {}
+  }
+}
+
+export const Admin = {
+  args: {
+    user: {
+      name: 'ヤマダ01',
+      permission: 'admin'
+    }
+  }
+}
+
+export const Staff = {
+  args: {
+    user: {
+      name: 'ヤマダ02',
+      permission: 'staff'
+    }
+  }
+}
+
+export const Guest = {
+  args: {
+    user: {
+      name: 'ヤマダ03',
+      permission: 'guest'
+    }
+  }
+}
---
+++ src/stories/userHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header
+    :class="
+      user.permission === 'admin'
+        ? 'red'
+        : user.permission === 'staff'
+          ? 'blue'
+          : user.permission === 'guest'
+            ? 'yellow'
+            : ''
+    "
+  >
+    <dl>
+      <dt>ユーザー名： {{ user.name }}</dt>
+      <dd>権限： {{ user.permission }}</dd>
+    </dl>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'userHeader',
+  props: {
+    user: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.red {
+  background-color: red;
+}
+.blue {
+  background-color: blue;
+}
+.guest {
+  background-color: yellow;
+}
+</style>
